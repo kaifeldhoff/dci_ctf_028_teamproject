bbf84c133bff036d90a0cad3912baa624c4da9ed
Hans-Juergen Grauer <Hans-Juergen.Grauer@dci-student.org>
2025-11-03 13:31:48 +0100

    first try

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
-// bilder verdecken css
-// bilder auswählen css
-// bilder ausgewählte sichtbar css
-// bilder vergleichen js
-// bilder wieder verdecken css
-// bilder vom pool entfernen  css
-// startknopof js/html
-// neustart knopf js/html
-const spielfeld = document.querySelector(".spielfeld");
-console.log(spielfeld);
-const cardArr = [
-  { name: "Apfel", img: "Apfel.png" },
-  { name: "Banane", img: "Banane.png" },
-];
-
-// const rueckseite = "rueckseite.png"; // Rückseite der Karten
-//  cardDiv.style.backgroundImage = `url('${rueckseite}')`;
-
-for (const card of cardArr) {
-  const cardDiv = document.createElement("div");
-  cardDiv.classList.add("karte");
-  cardDiv.textContent = card.name;
-  cardDiv.style.backgroundImage = `url('${images / himmel.jpg}')`;
-  spielfeld.appendChild(cardDiv);
+const tableBody = document.querySelector("#imageTable tbody");
+let html = "";
+for (let row = 0; row < 4; row++) {
+  html += "<tr>";
+  for (let col = 0; col < 4; col++) {
+    const index = row * 4 + col;
+    html += `<td><img src="./pub/pic${index}.jpg" alt="pic${index}" class="fixed-img"></td>`;
+  }
+  html += "</tr>";
 }
-console.table(spielfeld);
-// const a = document.querySelector(#a);
-// const b = document.querySelector(#b);
-// const c = document.querySelector(#c);
-// const d = document.querySelector(#d);
+tableBody.innerHTML = html;
 
-// if (a===b){
-//   console.log("nice")
-// } else {
-//   console.log("falsch")
-// }
-
-// if (c===d){
-//   console.log("nice")
-// } else {
-//   console.log("falsch")
-// }
+// Einheitliche Bildgröße setzen
+document.querySelectorAll(".fixed-img").forEach((img) => {
+  img.style.width = "100%";
+  img.style.height = "200px";
+  img.style.objectFit = "cover";
+});
