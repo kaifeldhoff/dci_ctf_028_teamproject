7febd95b8fb4dc61309f3358e010c2025fb9a0bd
Kai Feldhoff <kaifeldhoff@yahoo.de>
2025-11-03 13:39:02 +0100

    work-in-progress

--- index.js
+++ index.js
@@ -7,23 +7,61 @@
 // startknopof js/html
 // neustart knopf js/html
 const spielfeld = document.querySelector(".spielfeld");
-console.log(spielfeld);
-const cardArr = [
-  { name: "Apfel", img: "Apfel.png" },
-  { name: "Banane", img: "Banane.png" },
+spielfeld.classList.add("debug");
+const karten = [
+  { name: "Apfel", img: "" },
+  { name: "Banane", img: "" },
+  { name: "Birne", img: "" },
+  { name: "Orange", img: "" },
+  { name: "Pfirsich", img: "" },
+  { name: "Mango", img: "" },
+  { name: "Mandarine", img: "" },
+  { name: "Erdbeere", img: "" },
+  { name: "Apfel", img: "" },
+  { name: "Banane", img: "" },
+  { name: "Birne", img: "" },
+  { name: "Orange", img: "" },
+  { name: "Pfirsich", img: "" },
+  { name: "Mango", img: "" },
+  { name: "Mandarine", img: "" },
+  { name: "Erdbeere", img: "" },
 ];
 
-// const rueckseite = "rueckseite.png"; // Rückseite der Karten
-//  cardDiv.style.backgroundImage = `url('${rueckseite}')`;
+function mischen(array) {
+  const karten = array;
+  let gemischteKarten = [];
+  while (gemischteKarten.length < karten.length) {
+    const i = Math.floor(Math.random() * karten.length);
+    if (!karten[i].genommen) {
+      karten[i].genommen = true;
+      gemischteKarten.push(karten[i]);
+    }
+  }
 
-for (const card of cardArr) {
+  return gemischteKarten;
+}
+
+const gemischteKarten = mischen(karten);
+
+let ersteKarte = true;
+
+for (const karte of gemischteKarten) {
   const cardDiv = document.createElement("div");
   cardDiv.classList.add("karte");
-  cardDiv.textContent = card.name;
-  cardDiv.style.backgroundImage = `url('${images / himmel.jpg}')`;
+  cardDiv.classList.add("debug");
+  cardDiv.bild = karte.name;
+  cardDiv.style.backgroundImage = "url('images/himmel_90x90.jpg')";
   spielfeld.appendChild(cardDiv);
+
+  cardDiv.addEventListener("click", (e) => {
+    if (ersteKarte) {
+      // umdrehen
+    } else {
+      // umdrehen und auswerten
+    }
+  });
 }
-console.table(spielfeld);
+
 // const a = document.querySelector(#a);
 // const b = document.querySelector(#b);
 // const c = document.querySelector(#c);
